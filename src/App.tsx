2857701f961d1616f3823698b6cb5d4abc77dcd4
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-05-18 23:08:56 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <Button className='bg-red-700 hover:bg-red-600' size={'lg'}>Click me</Button>
+    </>
+  )
+}
+
+export default App
