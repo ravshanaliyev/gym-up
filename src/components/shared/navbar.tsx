d7cc79c7d90bb00a9694dfb386229fbc54a0649b
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-05-19 15:20:56 +0500

    navbar

---
+++ src/components/shared/navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
+
+const Navbar = () => {
+    return (
+        <div className='navbar bg-black py-7'>
+            <div className="container flex items-center justify-between">
+                <Link to={'/'}><img src="https://themewagon.github.io/fitnessclub/assets/img/logo/logo.png" alt="" /></Link>
+                <ul className='flex gap-8 items-center'>
+                    <li className='text-white hover:text-[#ff1313] transition text-[20px]'><Link to="/">Home</Link></li>
+                    <li className='text-white hover:text-[#ff1313] transition text-[20px]'><Link to="/">About</Link></li>
+                    <li className='text-white hover:text-[#ff1313] transition text-[20px]'><Link to="/">Classes</Link></li>
+                    <li className='text-white hover:text-[#ff1313] transition text-[20px]'><Link to="/">Gallery</Link></li>
+                    <li className='text-white hover:text-[#ff1313] transition text-[20px]'><Link to="/">Blog</Link></li>
+                    <li className='text-white hover:text-[#ff1313] transition text-[20px]'><Link to="/">Shop</Link></li>
+                    <li className='text-white hover:text-[#ff1313] transition text-[20px]'><Link to="/">Contact</Link></li>
+                    <Button className='rounded-none text-lg ' size={'lg'}>Became a Member</Button>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,18 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: "1rem",
       screens: {
-        "2xl": "1400px",
+        "2xl": "1450px",
       },
     },
     extend: {
@@ -74,4 +74,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
