45dade897c45e09d61004686200b6b88634d64a9
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-05-30 16:40:56 +0500

    I fixed some bugs in Home Page

--- src/components/shared/about-gym.tsx
+++ src/components/shared/about-gym.tsx
@@ -3,19 +3,19 @@ import { Button } from '../ui/button'
 const AboutGym = () => {
     return (
         <div className='container my-16'>
-            <div className="w-full lg:w-[1200px] mx-auto">
-                <div className="w-full block lg:flex items-center gap-10">
-                    <div className="w-full lg:w-1/2">
-                        <img className='w-full' src="https://themewagon.github.io/fitnessclub/assets/img/gallery/gallery2.png" alt="" />
+            <div className="w-full  mx-auto">
+                <div className="w-full  lg:flex items-center justify-center gap-x-20">
+                    <div className="w-full max-w-[570px]  h-[600px] ">
+                        <img className='w-full h-full object-cover' src="https://themewagon.github.io/fitnessclub/assets/img/gallery/gallery2.png" alt="" />
                     </div>
-                    <div className="w-full lg:w-1/2 mt-6 lg:mt-0">
+                    <div className="w-full  mt-6 lg:mt-0">
                         <div className="flex items-center gap-4 text-[#ff1313]">
                             <div className='h-[3px] w-[80px] bg-[#ff1313]'></div>
                             <h3 className='uppercase  text-2xl  teko'>About Me</h3>
                         </div>
-                        <h2 className='text-[44px] font-bold text-[#2c234d] leading-snug my-4 teko'>Safe Body building proper Solutions That Saves our Valuable Time!</h2>
-                        <p className='text-base text-[#212025] mb-4 barlow font-semibold'>Brook presents your services with flexible, convenient and cdpose layouts. You can select your favorite layouts & elements for cular ts with unlimited ustomization possibilities. Pixel-perfect replication of the designers is intended.</p>
-                        <p className='text-base text-[#212025] mb-4 barlow'>Brook presents your services with flexible, convefnient and chient anipurpose layouts. You can select your favorite layouts.</p>
+                        <h2 className='text-[44px] font-bold text-[#2c234d] leading-snug my-4 teko max-w-[520px]'>Safe Body building proper Solutions That Saves our Valuable Time!</h2>
+                        <p className='text-base text-[#212025] mb-4 barlow font-semibold max-w-[600px]'>Brook presents your services with flexible, convenient and cdpose layouts. You can select your favorite layouts & elements for cular ts with unlimited ustomization possibilities. Pixel-perfect replication of the designers is intended.</p>
+                        <p className='text-base text-[#212025] my-4 barlow max-w-[520px]'>Brook presents your services with flexible, convefnient and chient anipurpose layouts. You can select your favorite layouts.</p>
                         <Button className='rounded-none text-xl uppercase px-10 !py-6' size={'lg'}>Became a Member</Button>
                     </div>
                 </div>
--- src/components/shared/our-client-saying.tsx
+++ src/components/shared/our-client-saying.tsx
@@ -10,16 +10,16 @@ const OurClientSaying = () => {
     return (
         <div className="h-full py-6 lg:py-0  lg:h-[800px] flex flex-col justify-center">
             <div className="container">
-                <div className="w-full lg:w-[1200px] mx-auto block  md:flex items-center justify-between gap-2">
-                    <div className="w-full md:w-[480px] h-[620px]">
-                        <img className="w-full h-full object-cover" src="https://themewagon.github.io/fitnessclub/assets/img/gallery/about2.png" alt="" />
+                <div className="w-full lg:max-w-[1200px] mx-auto block  md:flex items-center justify-between gap-2">
+                    <div className="w-full md:max-w-[480px] h-[620px]">
+                        <img className="w-full h-full object-cover"  src="https://themewagon.github.io/fitnessclub/assets/img/gallery/about2.png" alt="" />
                     </div>
                     <div className="w-full mt-6 md:w-1/2 md:mt-0">
                         <div className="flex items-center justify-center lg:justify-start  gap-4 text-[#ff1313]">
                             <div className='h-[2px] w-[100px] bg-[#ff1313]'></div>
                             <h3 className='uppercase  text-[22px] teko'>CLIENT FEEDBACK</h3>
                         </div>
-                        <h3 className="text-[32px] md:text-[50px] my-4 font-bold  text-center lg:text-left teko">WHAT OUR CLIENT THIK ABOUT OUR GYM</h3>
+                        <h3 className="text-[32px]  md:text-[50px] my-4 font-bold  text-center lg:text-left teko">WHAT OUR CLIENT THIK ABOUT OUR GYM</h3>
                         <div className="relative">
                             <img className="absolute left-[-195px]" src="https://themewagon.github.io/fitnessclub/assets/img//gallery/qutaion.png" alt="" />
                         </div>
