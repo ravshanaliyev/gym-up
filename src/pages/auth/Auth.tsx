e7faf4047904337a9edb5bd3c33784830917c897
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-06-06 00:06:12 +0500

    test

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -15,13 +15,13 @@ const Auth = ({ openAuth, setOpenAuth }: { openAuth: boolean, setOpenAuth: Funct
 
   return (
     <Modal className=" flex justify-center items-center" aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description"
-    open={openAuth}
-    onClose={() => setOpenAuth(false)}
+      open={openAuth}
+      onClose={() => setOpenAuth(false)}
     >
       <div className="relative max-w-[500px] text-center w-full bg-[#fff] rounded-[6px] p-[1.4rem] ">
         <span onClick={() => setOpenAuth(false)} className="material-symbols-outlined absolute right-3 top-3 cursor-pointer">close</span>
         {
-          isRegistered ? <Verify/> : <Register setIsRegistered={setIsRegistered} isLogin={isLogin}  setIsLogin={setIsLogin} />
+          isRegistered ? <Verify /> : <Register setIsRegistered={setIsRegistered} isLogin={isLogin} setIsLogin={setIsLogin} />
         }
       </div>
     </Modal>
