dd878979c34ff7736935ed143ab857e5d256e832
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-06-11 09:03:25 +0500

    I fixed Auth

--- src/pages/auth/login/Login.tsx
+++ src/pages/auth/login/Login.tsx
@@ -8,6 +8,7 @@ const Login = () => {
     const [phoneNumber, setPhoneNumber] = useState("");
     const [password, setPassword] = useState("");
     const [focused, setFocused] = useState(false);
+    const [registerLoading, setRegisterLoading] = useState<boolean>(false)
     const [changeIcon, setChangeIcon] = useState(false);
     const navigate = useNavigate();
 
@@ -20,7 +21,7 @@ const Login = () => {
 
     const handleLogin = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
+        setRegisterLoading(true)
         const loginData: any = { phone: phoneNumber, password };
 
         mutate(loginData, {
@@ -28,13 +29,15 @@ const Login = () => {
                 if (res.statusCode === 200) {
                     const token = res.data || localStorage.getItem('token');
                     const role = token && jwtDecode(token);
+                    setTimeout(() => { setRegisterLoading(false) }, 2000)
                     const isAdmin = role["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
                     localStorage.setItem("token", res.data);
                     if (isAdmin === "Admin") {
-                        navigate("/admin/courses");
+                        setTimeout(() => { navigate("/admin/courses") }, 3000);
                     } else {
-                        // Redirect to a different page for non-admin users if needed
-                        navigate("/user/dashboard");
+                        setTimeout(() => { navigate("/user/dashboard") }, 3000)
+
+                            ;
                     }
                 }
             }
@@ -42,6 +45,7 @@ const Login = () => {
     };
 
     const handlePhoneNumberChange = (input_value: string) => {
+        setPhoneNumber(input_value)
         const pattern = /^\+998\s?\d{2}\s?\d{3}\s?\d{4}$/;
         if (pattern.test(input_value) || input_value === '') {
             setPhoneNumber(input_value);
@@ -56,10 +60,12 @@ const Login = () => {
     };
 
     const handleInputBlur = () => {
-        if (!phoneNumber.replace(/\s/g, '')) {
-            setPhoneNumber('');
-        }
-        setFocused(false);
+        console.log('');
+
+        // if (!phoneNumber.replace(/\s/g, '')) {
+        //     setPhoneNumber('');
+        // }
+        // setFocused(false);
     };
 
     return (
@@ -67,15 +73,19 @@ const Login = () => {
             <h3 className="text-center text-[32px] font-[500] text-[#fff] tracking-[2px]">Login</h3>
             <form onSubmit={handleLogin} className="w-full text-center">
                 <label className="text-[16px] text-[#fff] text-left mt-8 inline-block w-full tracking-[.5px] font-[400]" htmlFor="number">Phone Number
-                    <input placeholder="+998 XX XXX XXXX" onBlur={handleInputBlur} value={focused ? phoneNumber : ""} onFocus={handleInputFocus} onChange={(e) => handlePhoneNumberChange(e.target.value)} id="number" type="text" name="number" className=" bg-[#1b1b1b] p-[10px]    w-full h-[45px]  mt-[5px] outline-none indent-[5px] py-2  rounded-[6px]   " />
+                    <input required placeholder="+998 XX XXX XXXX" onBlur={handleInputBlur} value={focused ? phoneNumber : ""} onFocus={handleInputFocus} onChange={(e) => handlePhoneNumberChange(e.target.value)} id="number" type="text" name="number" className=" bg-[#1b1b1b] p-[10px]    w-full h-[45px]  mt-[5px] outline-none indent-[5px] py-2  rounded-[6px]   " />
                 </label>
                 <label className="relative text-[16px] text-[#fff] text-left mt-5 inline-block w-full tracking-[.5px] font-[400]" htmlFor="password">Password
-                    <input value={password} onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} id="password" type={passwordType} name="password" className="  bg-[#1b1b1b] p-[10px]    w-full h-[45px]  mt-[5px] outline-none indent-[5px] py-2  rounded-[6px] " />
+                    <input required value={password} onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} id="password" type={passwordType} name="password" className="  bg-[#1b1b1b] p-[10px]    w-full h-[45px]  mt-[5px] outline-none indent-[5px] py-2  rounded-[6px] " />
                     <span onClick={handleShowPassword} className="material-symbols-outlined absolute right-[2%] top-[60%] text-[20px] text-[#464545] cursor-pointer">{changeIcon ? "visibility_off" : "visibility"}</span>
                 </label>
 
-                <button type="submit" className="w-full  transition duration-[0.2s] bg-[#ff1414] rounded-[6px] text-[#fff] mt-[2rem]  text-[18px] font-[400] py-[5px] hover:bg-[#fa5757]">Login</button>
-
+                <div style={registerLoading ? { cursor: "not-allowed" } : { cursor: "pointer" }} className="w-full mt-8 transition duration-[0.2s]  bg-[#ff1414]  text-[#fff]   text-[16px] font-[400] py-[7px] rounded-[6px] hover:bg-[#fa5757]">
+                    {
+                        registerLoading ? <div className="register-loader"></div>
+                            : <button type="submit" className=" w-full h-full">Login</button>
+                    }
+                </div>
                 <Link to={"/auth/register"} className="mt-3 text-center inline-block font-[400] text-[#1752e0] m-auto hover:underline" >If you don't have an account, register here</Link>
             </form>
         </div>
