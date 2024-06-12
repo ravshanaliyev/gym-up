f0d14f7e2f4c5281745bdd5c28e0871df049197d
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-06-12 11:53:26 +0500

    user data update and pass updates are added

--- src/pages/admin-dashboard/settings.tsx
+++ src/pages/admin-dashboard/settings.tsx
@@ -1,6 +1,9 @@
+import UserPasswordUpdate from "@/components/shared/user-password-update"
+import UserUpdate from "@/components/shared/user-update"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useToast } from "@/components/ui/use-toast"
 import { useUpdatePassword } from "@/service/mutation/useUpdatePassword"
 import { useUpdateUser } from "@/service/mutation/useUpdateUser"
 import { useGetUser } from "@/service/query/useGetUser"
@@ -8,35 +11,46 @@ import { jwtDecode } from "jwt-decode"
 import { useForm } from "react-hook-form"
 
 const Settings = () => {
+    const { toast } = useToast()
     const token = localStorage.getItem('token')
     const user: any = token && jwtDecode(token!)
-    const { register, handleSubmit } = useForm()
     const { mutate, isPending } = useUpdateUser()
     const { mutate: mutatePass } = useUpdatePassword()
     const { data, isLoading } = useGetUser(user.Id)
     const onSubmit = (data: any) => {
-        // const formdata = new FormData()
-        // formdata.append('id', user.Id)
-        // formdata.append('firstname', data.firstName)
-        // formdata.append('lastname', data.lastName)
-        // formdata.append('phone', data.phone)
-        // formdata.append('isPayed', true)
         mutate({ ...data, id: Number(user.Id), isPayed: true }, {
             onSuccess: (res) => {
                 console.log(res);
+                toast({
+                    title: 'Success',
+                    description: 'User updated successfully',
+                })
             },
             onError: (error) => {
                 console.log(error);
+                toast({
+                    title: 'Error',
+                    description: error.message,
+                })
             }
         })
     }
     const onSubmitPassword = (values: any) => {
-        mutatePass({ id: Number(user.Id), oldPass: values.oldPass, newPass: values.newPass }, {
+        const { oldPass, newPass } = values
+        mutatePass({ oldPass, newPass }, {
             onSuccess: (res) => {
                 console.log(res);
+                toast({
+                    title: 'Success',
+                    description: 'Password updated successfully',
+                })
             },
             onError: (error) => {
                 console.log(error);
+                toast({
+                    title: 'Error',
+                    description: error.message,
+                })
             }
         })
     }
@@ -45,50 +59,10 @@ const Settings = () => {
             <div className="flex flex-col gap-6">
                 {
                     isLoading ? <h3>Loading...</h3> : (
-                        <form onSubmit={handleSubmit(onSubmit)} className="border rounded-xl p-5 shadow bg-white">
-                            <h3 className="text-xl">Personal Information</h3>
-                            <p className="text-sm">Details about your personal information</p>
-                            <hr className="my-5" />
-                            <div className="block md:flex gap-6 my-6">
-                                <div className="w-full md:w-1/2">
-                                    <Label htmlFor="phone">Phone</Label>
-                                    <Input {...register('phone')} id="phone" type="text" defaultValue={data?.data?.phone} />
-                                </div>
-                            </div>
-                            <div className="block md:flex gap-6 my-6 ">
-                                <div className="w-full md:w-1/2">
-                                    <Label htmlFor="firstName">First Name</Label>
-                                    <Input {...register('firstName')} defaultValue={data?.data?.firstname} id="firstName" />
-                                </div>
-                                <div className="w-full md:w-1/2 mt-4 md:mt-0">
-                                    <Label htmlFor="lastName">Last Name</Label>
-                                    <Input {...register('lastName')} defaultValue={data?.data?.lastname} id="lastName" />
-                                </div>
-                            </div>
-                            <div className="flex justify-end">
-                                <Button variant={'outline'} type="submit" disabled={isPending}>Save Changes</Button>
-                            </div>
-                        </form>
+                        <UserUpdate submit={onSubmit} data={data} isPending={isPending} />
                     )
                 }
-                <form onSubmit={handleSubmit(onSubmitPassword)} className="border rounded-xl p-5 shadow bg-white">
-                    <h3 className="text-xl">Change Password</h3>
-                    <p className="text-sm">Details about your account password change</p>
-                    <hr className="my-5" />
-                    <div className="block md:flex gap-6 my-6">
-                        <div className="w-full md:w-1/3">
-                            <Label htmlFor="currentPassword">Current Password</Label>
-                            <Input {...register('oldPass')} type="password" placeholder="Current Password" id="currentPassword" />
-                        </div>
-                        <div className="w-full md:w-1/3 mt-4 md:mt-0">
-                            <Label htmlFor="newPassword">New Password</Label>
-                            <Input {...register('newPass')} type="password" placeholder="New Password" id="newPassword" />
-                        </div>
-                    </div>
-                    <div className="flex justify-end">
-                        <Button type="submit" variant={'outline'}>Save Changes</Button>
-                    </div>
-                </form>
+                <UserPasswordUpdate onSubmitPassword={onSubmitPassword} />
             </div>
         </div>
     )
--- src/service/mutation/useUpdatePassword.ts
+++ src/service/mutation/useUpdatePassword.ts
@@ -5,7 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 export const useUpdatePassword = () => {
     return useMutation({
         mutationFn: (data: any) => {
-            return ApiInctance.put("/users/update-password", data).then(res => res.data)
+            return ApiInctance.put(`/users/update-password?oldPass=${data.oldPass}&newPass=${data.newPass}`).then(res => res.data)
         }
     })
 }
