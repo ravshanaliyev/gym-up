3fe3e214cee7ccff1ab9a602d7357903e293dd16
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-06-16 18:41:29 +0500

    update course

--- src/components/shared/update-course.tsx
+++ src/components/shared/update-course.tsx
@@ -2,7 +2,6 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-import { useForm } from "react-hook-form";
 import { useUpdateCourse } from "@/service/mutation/useUpdateCourse";
 import { client } from "@/service/QueryClient";
 import { useToast } from "../ui/use-toast";
@@ -11,31 +10,49 @@ import { CourseType } from "@/@types/types";
 import { useEffect, useState } from "react";
 
 const UpdateCourse = ({ course, isOpen, setIsOpen }: { course: CourseType, isOpen: boolean, setIsOpen: Function }) => {
-    // const [isOpen, setIsOpen] = useState<boolean>(false);
     console.log(isOpen);
-    
+
     const { toast } = useToast();
-    const { handleSubmit, register, reset } = useForm();
     const { mutate: updateCourse } = useUpdateCourse();
 
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
-
+    const [image, setImage] = useState('')
 
     useEffect(() => {
         setTitle(course.title)
         setDescription(course.description)
+        setImage(course.image)
     }, [course])
 
     const { id } = course;
-    const updatedCourse = {id, title, description}
+    const updatedCourse = { id, title, description, image }
 
-    const onUpdateSubmit = (value: any) => {
-        updateCourse({ ...value, id }, {
-            onSuccess: () => {
+    // const onUpdateSubmit = (value: any) => {
+    //     updateCourse({ ...value, id }, {
+    //         onSuccess: () => {
+    //             client.invalidateQueries({ queryKey: ['get-courses'] });
+    //             setIsOpen(false);
+    //             toast({
+    //                 title: "Course updated successfully",
+    //                 description: "You can add more courses",
+    //             });
+    //         },
+    //         onError: (error) => {
+    //             console.log(error);
+    //         }
+    //     });
+    // };
+
+
+    const handleUpdateCourse = (e: Event) => {
+        e.preventDefault()
+        console.log(updatedCourse);
+        updateCourse(updatedCourse, {
+            onSuccess: (res) => {
+                console.log(res);
                 client.invalidateQueries({ queryKey: ['get-courses'] });
                 setIsOpen(false);
-                reset()
                 toast({
                     title: "Course updated successfully",
                     description: "You can add more courses",
@@ -43,28 +60,16 @@ const UpdateCourse = ({ course, isOpen, setIsOpen }: { course: CourseType, isOpe
             },
             onError: (error) => {
                 console.log(error);
-            }
-        });
-    };
-
-
-    const handleUpdateCourse = (e: Event) => {
-        e.preventDefault()
-        updateCourse(updatedCourse, {
-            onSuccess:(res) => {
-                console.log(res);
-                client.invalidateQueries({ queryKey: ['get-courses'] });
-                setIsOpen(false);
-            },
-            onError: (error) => {
-                console.log(error);
-                
+                toast({
+                    title: "There was an error",
+                    description: error.message,
+                });
             }
         })
     }
 
     return (
-        <div style={ isOpen ?{display: 'none'} : {display: 'none'}}>
+        <div style={isOpen ? { display: 'none' } : { display: 'none' }}>
             <Dialog open={isOpen} onOpenChange={setIsOpen as any}>
                 <DialogTrigger asChild>
                     <Button className="bg-[#3c50e0] h-9 w-9 hover:bg-[#3c50e0] hover:bg-opacity-90 text-white" size="icon">
@@ -76,11 +81,17 @@ const UpdateCourse = ({ course, isOpen, setIsOpen }: { course: CourseType, isOpe
                     <form onSubmit={handleUpdateCourse as any} className="flex flex-col gap-6">
                         <div className="flex flex-col gap-2">
                             <Label htmlFor="title">Name</Label>
-                            <Input type="text" id="title" value={title}   onChange={(e) => setTitle(e.target.value)}  />
+                            <Input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
                         </div>
                         <div className="flex flex-col gap-2">
                             <Label htmlFor="description">Description</Label>
                             <Input type="text" id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <Label htmlFor="name">Image</Label>
+                            <Input type="file" id="name" onChange={(e) =>
+                                // @ts-ignore
+                                setImage(e.target.files[0])} />
                         </div>
                         <Button type="submit" className="bg-[#3C50E0] h-[40px] hover:bg-[#5162e2]">Submit</Button>
                     </form>
--- src/service/mutation/useUpdateCourse.ts
+++ src/service/mutation/useUpdateCourse.ts
@@ -6,8 +6,12 @@ export const useUpdateCourse = () => {
     return useMutation({
         mutationKey: ['update-course'],
         mutationFn: (data: any) => {
-            return ApiInctance.put("/courses/update", data).then(res => res.data)
-            .catch((error) => console.log(error))
+            return ApiInctance.put(`/courses/update?Id=${data.id}&Title=${data.title}&Description=${data.description}`, data, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }).then(res => res.data)
+                .catch((error) => console.log(error))
         }
     })
 }
