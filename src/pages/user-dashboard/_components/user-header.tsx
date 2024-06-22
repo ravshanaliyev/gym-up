4e411df9fe2d6e90919c0542413865ed8994bf3c
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-06-22 06:45:33 +0500

    responsive header

--- src/pages/user-dashboard/_components/user-header.tsx
+++ src/pages/user-dashboard/_components/user-header.tsx
@@ -1,6 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger, } from "@/components/ui/dropdown-menu"
-import { useNavigate } from "react-router-dom"
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { CirclePlay, SquareMenu, User } from "lucide-react"
+import { Link, useNavigate } from "react-router-dom"
+import UserSidebarItem from "./user-sidebar-item"
+import { useGetCourses } from "@/service/query/useGetCourses"
 
 
 const UserHeader = () => {
@@ -9,9 +13,37 @@ const UserHeader = () => {
         localStorage.removeItem("token")
         navigate("/auth/login")
     }
+
+    const { data } = useGetCourses()
     return (
         <div className='w-full h-[80px] shadow-md flex items-center barlow bg-white  justify-between px-8'>
-            <h3 className="text-black text-center text-xl barlow font-semibold">Gym Up</h3>
+            <div className='flex items-center gap-2 lg:hidden '>
+                <Sheet>
+                    <SheetTrigger>
+                        <SquareMenu className='cursor-pointer block lg:hidden' />
+                    </SheetTrigger>
+                    <SheetContent side={'left'} className='bg-[#1c2434] text-white'>
+                        <div className="flex flex-col space-y-4 mt-8">
+                            <h3 className="text-white font-bold text-[16px] text-center">Courses</h3>
+                            {data?.data.map((link: any) => (
+                                <SheetClose asChild key={link.id}>
+                                    <Link to={`/user-dashboard/courses/${link.id}`}>
+                                        <UserSidebarItem icon={CirclePlay} label={link.title} path={`/user-dashboard/courses/${link.id}`} />
+                                    </Link>
+                                </SheetClose>
+                            ))}
+                            <h3 className="text-white font-bold text-[16px] text-center">Account</h3>
+                            <SheetClose asChild>
+                                <Link to="/user-dashboard/account">
+                                    <UserSidebarItem icon={User} label="Account" path="/user-dashboard/account" />
+                                </Link>
+                            </SheetClose>
+                        </div>
+                    </SheetContent>
+                </Sheet>
+                <h3 className="text-black text-center text-xl barlow font-semibold">Gym Up</h3>
+            </div>
+            <h3 className="hidden lg:block text-black text-center text-xl barlow font-semibold">Gym Up</h3>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <div className="flex items-center gap-2 cursor-pointer">
@@ -27,6 +59,13 @@ const UserHeader = () => {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent className="w-56">
                     <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <Link to={'/user-dashboard/account'}>
+                        <DropdownMenuItem>
+                            Account
+                            <DropdownMenuShortcut>⇧⌘K</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                    </Link>
                     <DropdownMenuSeparator />
                     <DropdownMenuItem onClick={handleLogout}>
                         Log out
