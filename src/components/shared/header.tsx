239a93230ea591ebe427ff04df367f76905f86e8
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-07-01 14:12:10 +0500

    header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -11,9 +11,9 @@ const Header = () => {
     }, [])
 
     return (
-        <div className='relative h-[600px] md:h-[640px] bg-black w-full flex justify-start items-center bg-fixed ' style={{ backgroundImage: resize < 480 ? 'url("https://themewagon.github.io/fitnessclub/assets/img/gallery/gallery2.png")' : 'url("https://themewagon.github.io/fitnessclub/assets/img/hero/h1_hero.png")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }} >
+        <div className='relative h-[600px] md:h-[640px] bg-black w-full flex justify-start items-center bg-fixed ' style={{ backgroundImage: resize < 450 ? 'url("https://themewagon.github.io/fitnessclub/assets/img/gallery/gallery2.png")' : 'url("https://themewagon.github.io/fitnessclub/assets/img/hero/h1_hero.png")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }} >
             {
-                resize < 480 && <div className="absolute inset-0   h-[600px]  w-full" style={{ background: `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7))`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                resize < 450 && <div className="absolute inset-0   h-[600px]  w-full" style={{ background: `linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7))`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
             }
             <div className="container">
                 <div className=" flex flex-col justify-start w-full  md:w-[670px] md:ml-12">
